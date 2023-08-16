1d394fa6fa372d8bb76ea2c1be62a64bcb122c10
zarlipzz <guitar30255@gmail.com>
2023-08-17 02:14:53 +0700

    workk

--- Homework/HW06/hw06-2-roman-number.cpp
+++ Homework/HW06/hw06-2-roman-number.cpp
@@ -23,3 +23,24 @@
         929 = CMXXIX
 
 */
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+
+    printf("%d = ", num);
+
+    int decimals[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    const char *roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    for (int i = 0; num > 0; i++) {
+        while (num >= decimals[i]) {
+            printf("%s", roman[i]);
+            num -= decimals[i];
+        }
+    }
+
+    return 0;
+}
